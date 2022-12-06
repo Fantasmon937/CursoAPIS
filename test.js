ff118a0f258784ad2b2e01379157b19a94c29960
Fantasmon937 <pablo991004.lalo@gmail.com>
2022-12-06 10:36:17 -0600

    Agregar cosas

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+
+let a = 10;
+
+const b = 10;
+
+if (a == b) {
+    let a = 1;
+    console.log(a);
+}
+console.log(a);
+
+//Arrays
+let c = [1,2,3,4,5,6,7,8,9,10];
+console.log(c);
